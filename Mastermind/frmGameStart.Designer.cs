0cc2e3eea8b76574481ec4ca97a8502f93db2d77
Nkem Ohanenye <tuj20157@temple.edu>
2020-03-18 15:08:19 -0400

    intial commit

---
+++ Mastermind/frmGameStart.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Mastermind
+{
+    partial class frmGameStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(34, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to The Mastermind Game!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(55, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play Mastermind";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(380, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Play Ultimate";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(219, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "How To Play";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // frmGameStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 389);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGameStart";
+            this.Text = "Welcome to Mastermind";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
