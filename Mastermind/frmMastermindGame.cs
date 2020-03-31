305a44f7e8911bd156d8bd29c117c503a0a7be1d
Nkem Ohanenye <tuj20157@temple.edu>
2020-03-31 17:14:50 -0400

    Major changes to the board, the game now checks if you lose through max guesses, only checks if all buttons have a color and the current row is mainly implemented

--- Mastermind/frmMastermindGame.cs
+++ Mastermind/frmMastermindGame.cs
@@ -16,33 +16,44 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-//Notes: for the hints, red means the color is a correct color and in correct position, white means the color is correct but in the wrong position, black if wrong
+// Notes: for the hints, red means the color is a correct color and in correct position, 
+// white means the color is correct but in the wrong position, black if wrong
 namespace Mastermind
 {
     public partial class frmMastermindGame : Form
     {
-        private const int CODELENGTH = 4;  //the size of the answer (number of colored marbles, or colored buttons in our case, in the answer); An option we would like to incorporate: player chooses the codeLength.
-        private const int NUMROWS = 10;    //number of rows for the main board and check board; default is 10
+        private const int CODELENGTH = 4;  // the size of the answer (number of colored marbles, or colored buttons in our case, in the answer); An option we would like to incorporate: player chooses the codeLength.
+        private const int NUMROWS = 10;    // number of rows for the main board and check board; default is 10
 
-        private Button[] answerBoard = new Button[CODELENGTH]; //an array of buttons of size codeLength
-        private Button[,] mainBoard = new Button[NUMROWS, CODELENGTH];  //2d array of buttons representing the board where the player will place his/her guesses
-        private Button[,] checkBoard = new Button[NUMROWS, CODELENGTH]; //2d array of buttons representing the board which will give hints to indicate whether the player’s guessed colors are correct and in the right position
-        private Button[,] givenColorsBoard = new Button[2, 4];   //2d array of buttons with two rows of 4 to represent the given colors the player can choose from
+        private Button[] answerBoard = new Button[CODELENGTH]; // an array of buttons of size codeLength
+        private Button[,] mainBoard = new Button[NUMROWS, CODELENGTH];  // 2d array of buttons representing the board where the player will place his/her guesses
+        private Button[,] checkBoard = new Button[NUMROWS, CODELENGTH]; // 2d array of buttons representing the board which will give hints to indicate whether the player’s guessed colors are correct and in the right position
+        private Button[,] givenColorsBoard = new Button[2, 4];   // 2d array of buttons with two rows of 4 to represent the given colors the player can choose from
 
-        private Size size = new Size(23, 23);
-        private int padding = 30;
-        private Point loc = new Point(0, 15);
+        // an array of buttons that acts as the currently active row
+        private Button[] currPlayerRow = new Button[CODELENGTH];
+        // an array of buttons that acts as the previously active row
+        // used to set the previous row disabled
+        private Button[] prevPlayerRow = new Button[CODELENGTH];
+        private int colIndex = 0; // gets the index to the current index the currPlayerRow is pointing to
+        private int currPlayerIndex = 0; // the index for what row the current player is at on the board
+
+        private Size size = new Size(23, 23); // sets the size for the buttons 
+        private int padding = 30; // sets the padding around the buttons
+        private Point loc = new Point(0, 15); // sets the x,y coordinates for the buttons on the panels
+
+        private ColorsClass color = new ColorsClass();
 
 
-        //Constructor that creates a new game form
+        // Constructor that creates a new game form
         public frmMastermindGame()
         {
             InitializeComponent();
         }
 
 
-        //Upon loading the form, the buttons in each "board" (answerBoard, mainBoard, checkBoard, and givenColorsBoard)
-        //Are created and displayed in their respective panels
+        // Upon loading the form, the buttons in each "board" (answerBoard, mainBoard, checkBoard, and givenColorsBoard)
+        // Are created and displayed in their respective panels
         public void frmMastermindGame_Load(object sender, EventArgs e)
         {
             createAnswerBoard();
@@ -51,8 +62,9 @@ namespace Mastermind
             createColorsBoard();
         }
 
-        //Creates the array of buttons of length codeLength; sets the background color of each button to be the colors from hiddenAnswer of the Computer class
-        //Initializes them to be visible = false until the player either wins or uses up all of his guesses
+        // Creates the array of buttons of length codeLength; sets the background color of each button to be 
+        // the colors from hiddenAnswer of the Computer class
+        // Initializes them to be visible = false until the player either wins or uses up all of his guesses
         public void createAnswerBoard()
         {
             for (int col = 0; col < CODELENGTH; col++)
@@ -67,8 +79,9 @@ namespace Mastermind
             //pnlAnswerBoard.Visible = false;
         }
 
-        //Creates a 2d array of row x col buttons and displays these buttons on the main panel on the form;
-        //Disables all of the buttons except the buttons in the first row (the first row buttons will be the initial currentPlayerRow)
+        // Creates a 2d array of row x col buttons and displays these buttons on the main panel on the form;
+        // Disables all of the buttons except the buttons in the first row (the first row buttons will be the 
+        // initial currentPlayerRow)
         public void createMainBoard()
         {
             for (int row = 0; row < NUMROWS; row++)
@@ -79,16 +92,55 @@ namespace Mastermind
                     mainBoard[row, col] = new Button();
                     mainBoard[row, col].Location = new Point(padding + col * (size.Width + padding), loc.Y);
                     mainBoard[row, col].Size = size;
-                    //mainBoard[row, col].Enabled = false;
+                    mainBoard[row, col].Enabled = false;
                     mainBoard[row, col].Name = "btnMain" + row.ToString() + col.ToString();
                     pnlMainBoard.Controls.Add(mainBoard[row, col]);
                     mainBoard[row, col].Click += new EventHandler(MainButton_Click);
                 }
             }
+            
+            // assigns the original current row
+            for (int col = 0; col < CODELENGTH; col++)
+            {
+                currPlayerRow[colIndex] = mainBoard[currPlayerIndex, col];
+                currPlayerRow[colIndex].Enabled = true;
+                colIndex++;
+            }
         }
 
-        //Creates a 2d array of row x col buttons and displays these buttons in the panel to the right of the main board
-        //All buttons are disabled and the background colors of all of them are initialized to be black (the first row buttons will be the initial currentCheckRow)
+        // used to call a method to get the next row, 
+        // if the index has the same value of rows, then the player loses, 
+        // you cant go to the 11th row if there are only 10 
+        public void nextPlayerRow()
+        {
+            if (currPlayerIndex == NUMROWS - 1)
+            {
+                MessageBox.Show("You Lost");
+                // Player loses
+            }
+            else
+            {
+                // disable the last row
+                colIndex = 0;
+                prevPlayerRow = currPlayerRow;
+                for (int col = 0; col < CODELENGTH; col++)
+                    prevPlayerRow[colIndex++].Enabled = false;
+
+                // goes to the next row
+                colIndex = 0;
+                currPlayerIndex++;
+                for (int col = 0; col < CODELENGTH; col++)
+                {
+                    currPlayerRow[colIndex] = mainBoard[currPlayerIndex, col];
+                    currPlayerRow[colIndex].Enabled = true;
+                    colIndex++;
+                }
+            }
+        }
+
+        // Creates a 2d array of row x col buttons and displays these buttons in the panel to the right of the main board
+        // All buttons are disabled and the background colors of all of them are initialized 
+        // to be black (the first row buttons will be the initial currentCheckRow)
         public void createCheckBoard()
         {
             size = new Size(17, 23);
@@ -108,7 +160,8 @@ namespace Mastermind
             }
         }
 
-        //Creates the 2d array of 2 rows of 4 buttons and initializes each of the button colors to be a color from givenColors in the Colors class
+        // Creates the 2d array of 2 rows of 4 buttons and initializes each of the button 
+        // colors to be a color from givenColors in the Colors class
         public void createColorsBoard()
         {
             ColorsClass given = new ColorsClass();
@@ -131,38 +184,59 @@ namespace Mastermind
             }
         }
 
-        //Calls on the isMatch method of the Computer Class to see if player's guess matches the hiddenAnswer
-        //Displays the colored hints in the currentCheckRow if all of the board colors for currentPlayerRow have been set
-        //If the row is not finished, player will be asked to finish the row
-        public void btnCheck_Click(object sender, EventArgs e)
-        {
-
-        }
-
         // Convert a charcter to its equivalent integer
         int convertCharToInt(char c)
         {
             return ((int)(c) - (int)('0'));
         } // end convertCharToInt
 
-        //Button click event for all buttons in the givenColorsBoard (to find which colored button is clicked)
+        // Button click event for all buttons in the givenColorsBoard (to find which colored button is clicked)
         private void GivenButton_Click(object sender, EventArgs e)
         {
             int rowID = convertCharToInt(((Button)sender).Name[8]);
             int colID = convertCharToInt(((Button)sender).Name[9]);
-            //MessageBox.Show("Cell[" + rowID + "," + colID + "] has been selected!");
-            ColorsClass color = new ColorsClass();
             color.setColorPicked(givenColorsBoard[rowID, colID].BackColor);
         }
         
-        //Button click event for all of the buttons in mainBoard (to find which button on the board that the player clicked)
+        // Button click event for all of the buttons in mainBoard (to find which button on the board that the player clicked)
         private void MainButton_Click(object sender, EventArgs e)
         {
             int rowID = convertCharToInt(((Button)sender).Name[7]);
             int colID = convertCharToInt(((Button)sender).Name[8]);
-            //MessageBox.Show("Cell[" + rowID + "," + colID + "] has been selected!");
-            ColorsClass color = new ColorsClass();
             mainBoard[rowID, colID].BackColor = color.getColorPicked();
+        }
+
+        // Calls on the isMatch method of the Computer Class to see if player's guess matches the hiddenAnswer
+        // Displays the colored hints in the currentCheckRow if all of the board colors for currentPlayerRow have been set
+        // If the row is not finished, player will be asked to finish the row
+        public void btnCheck_Click(object sender, EventArgs e)
+        {
+            Boolean validCheck = true;
+            colIndex = 0;
+            for (int col = 0; col < CODELENGTH; col++) {
+                if (currPlayerRow[colIndex].BackColor == null || 
+                    currPlayerRow[colIndex].BackColor == Color.Empty ||
+                    currPlayerRow[colIndex].BackColor == SystemColors.Control)
+                {
+                    MessageBox.Show("Your current row needs to have a color selected for each button.");
+                    validCheck = false;
+                    break;
+                }
+                colIndex++;
+            }
+            if (validCheck == true)
+            {
+                Computer com = new Computer(CODELENGTH);
+                if (com.isMatch(currPlayerRow))
+                {
+                    MessageBox.Show("You Win");
+                    // Player wins
+                }
+                else
+                {
+                    nextPlayerRow();
+                }
+            }
         }
 
         //Closes the form when user clicks Exit button
