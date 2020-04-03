4430d7a29d171ae459c64c9a514bb3511fb96ad5
Nkem Ohanenye <tuj20157@temple.edu>
2020-04-02 22:31:32 -0400

    Added the ability to change the code length and row amount, deleted the ultimate game, and fixed all previous merge errors

--- Mastermind/frmGameStart.Designer.cs
+++ Mastermind/frmGameStart.Designer.cs
@@ -31,27 +31,24 @@
             this.lblWelcomeToMastermind = new System.Windows.Forms.Label();
             this.btnPlayMastermind = new System.Windows.Forms.Button();
             this.btnHowToPlay = new System.Windows.Forms.Button();
-            this.btnPlayUltimate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblWelcomeToMastermind
             // 
             this.lblWelcomeToMastermind.AutoSize = true;
             this.lblWelcomeToMastermind.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lblWelcomeToMastermind.Location = new System.Drawing.Point(42, 135);
-            this.lblWelcomeToMastermind.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblWelcomeToMastermind.Location = new System.Drawing.Point(24, 68);
             this.lblWelcomeToMastermind.Name = "lblWelcomeToMastermind";
-            this.lblWelcomeToMastermind.Size = new System.Drawing.Size(1104, 76);
+            this.lblWelcomeToMastermind.Size = new System.Drawing.Size(560, 39);
             this.lblWelcomeToMastermind.TabIndex = 0;
             this.lblWelcomeToMastermind.Text = "Welcome to The Mastermind Game!";
             // 
             // btnPlayMastermind
             // 
             this.btnPlayMastermind.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnPlayMastermind.Location = new System.Drawing.Point(152, 510);
-            this.btnPlayMastermind.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnPlayMastermind.Location = new System.Drawing.Point(209, 258);
             this.btnPlayMastermind.Name = "btnPlayMastermind";
-            this.btnPlayMastermind.Size = new System.Drawing.Size(380, 71);
+            this.btnPlayMastermind.Size = new System.Drawing.Size(190, 37);
             this.btnPlayMastermind.TabIndex = 1;
             this.btnPlayMastermind.Text = "Play Mastermind";
             this.btnPlayMastermind.UseVisualStyleBackColor = true;
@@ -60,37 +57,22 @@
             // btnHowToPlay
             // 
             this.btnHowToPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnHowToPlay.Location = new System.Drawing.Point(400, 335);
-            this.btnHowToPlay.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnHowToPlay.Location = new System.Drawing.Point(209, 164);
             this.btnHowToPlay.Name = "btnHowToPlay";
-            this.btnHowToPlay.Size = new System.Drawing.Size(380, 71);
+            this.btnHowToPlay.Size = new System.Drawing.Size(190, 37);
             this.btnHowToPlay.TabIndex = 3;
             this.btnHowToPlay.Text = "How To Play";
             this.btnHowToPlay.UseVisualStyleBackColor = true;
             this.btnHowToPlay.Click += new System.EventHandler(this.btnHowToPlay_Click);
             // 
-            // btnPlayUltimate
-            // 
-            this.btnPlayUltimate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnPlayUltimate.Location = new System.Drawing.Point(656, 510);
-            this.btnPlayUltimate.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.btnPlayUltimate.Name = "btnPlayUltimate";
-            this.btnPlayUltimate.Size = new System.Drawing.Size(380, 71);
-            this.btnPlayUltimate.TabIndex = 4;
-            this.btnPlayUltimate.Text = "Play Ultimate";
-            this.btnPlayUltimate.UseVisualStyleBackColor = true;
-            this.btnPlayUltimate.Click += new System.EventHandler(this.btnPlayUltimate_Click);
-            // 
             // frmGameStart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1212, 748);
-            this.Controls.Add(this.btnPlayUltimate);
+            this.ClientSize = new System.Drawing.Size(606, 389);
             this.Controls.Add(this.btnHowToPlay);
             this.Controls.Add(this.btnPlayMastermind);
             this.Controls.Add(this.lblWelcomeToMastermind);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "frmGameStart";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome to Mastermind";
@@ -104,7 +86,6 @@
         private System.Windows.Forms.Label lblWelcomeToMastermind;
         private System.Windows.Forms.Button btnPlayMastermind;
         private System.Windows.Forms.Button btnHowToPlay;
-        private System.Windows.Forms.Button btnPlayUltimate;
     }
 }
 
