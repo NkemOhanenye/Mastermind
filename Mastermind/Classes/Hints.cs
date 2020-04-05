c64e551729d5aac9c1d048f164835e0336f9453a
Tracy <tlant300@gmail.com>
2020-04-05 17:11:06 -0400

    changed hints algorithm to use Dictionaries to keep track of counts for colors. will work for duplicates

--- Mastermind/Classes/Hints.cs
+++ Mastermind/Classes/Hints.cs
@@ -24,7 +24,8 @@ namespace Mastermind.Classes
         private Button[] currentCheckRow;
         private int numPerfectGuess = 0;       //counts number of colors that are correct and in correct position
         private int numOKGuess = 0;          //counts number of colors tht are correct but in the wrong position
-        private Color[] colorsSeen;        //keeps track of colors that have been seen before in currentPlayerRow
+        private Dictionary<Color, int> countForAnswer = new Dictionary<Color, int>(); //gives each color in hiddenAnswer a count value (useful for when there's duplicate colors)
+        private Dictionary<Color, int> countForCurrRow = new Dictionary<Color, int>();  //gives each color in currentPlayerRow a count value (aka how many times the player placed color)
 
         //Constructor 
         public Hints()
@@ -36,68 +37,112 @@ namespace Mastermind.Classes
         public void setCheckRow(Button[] currHintRow)
         {
             currentCheckRow = currHintRow;
-            colorsSeen = new Color[currentCheckRow.Length];
+      
         }
 
 
-        //Loops through the colors in currentPlayerRow and increases either numPerfectGuess or numOKGuess
-        //If a color appears in colorsSeen, neither of the counts are incremented as this color is a duplicate
-        //Returns a count of the number of perfect guesses
+        //Goes through currentPlayerRow and hiddenAnswer and places the colors in Dictionaries along with a count for each color
+        //Increments numOKGuess for every color that is in currentPlayerRow and in hiddenAnswer
+        //If a color in currentPlayerRow matches the position of the same color in hiddenAnswer, numPerfectGuess is incremented
+        //Returns the count value for numPerfectGuess
         public int countGuesses(Color[] hiddenAnswer, Button[] currentPlayerRow)
         {
-            int i = 0;
-            int sameColorCount = 1;
-            Color first = currentPlayerRow[0].ForeColor;
-
-            for (int j = 1; j < currentPlayerRow.Length; j++)
+            if (countForAnswer.Count == 0)         //if the count of the colors in hidden answer is empty, populate it
             {
-                if (first.Equals(currentPlayerRow[j].ForeColor))
-                {
-                    sameColorCount++;
-                }
+                populateCountForAnswer(hiddenAnswer);
             }
+            populateCountForCurrRow(currentPlayerRow);
 
-            //if all the colors in the player's guess are the same and this color is in the answer,
-            //increment numPerfectGuess by one since one of those positions is correct
-            if (sameColorCount == currentPlayerRow.Length)
+            Color first = currentPlayerRow[0].ForeColor;
+            int count;
+            countForCurrRow.TryGetValue(first, out count);
+
+            //increments numPerfectGuess by 1 if all colors in currentPlayerRow are the same AND this color is in hiddenAnswer
+            if (count == currentPlayerRow.Length && hiddenAnswer.Contains(first))
             {
-                if (hiddenAnswer.Contains(first))
-                {
-                    numPerfectGuess++;
-                }
+                numPerfectGuess++;
             }
             else
             {
+                //increment numOKGuess for every color that the player has that is in hiddenAnswer
+                //ex: if player places exactly two blues and hiddenAnswer contains exactly two blues, numOKGuess increments by 2
+                //ex: if player places exactly two blues but hiddenAnswer contains only one blue, numOKGuess increments by 1
+                //incrementation based on the count value of the color from countForAnswer
+                //if player's count for a color is less, numOKGuess would increment by the count value from countForCurrRow instead
+                Dictionary<Color, int>.KeyCollection playerColors = countForCurrRow.Keys;
+                foreach (Color currentColor in playerColors)
+                { 
+                    if (hiddenAnswer.Contains(currentColor) && countForAnswer.ContainsKey(currentColor))
+                    {
+                        if (countForCurrRow[currentColor] == countForAnswer[currentColor] 
+                            || countForCurrRow[currentColor] > countForAnswer[currentColor])
+                        {
+                            numOKGuess += countForAnswer[currentColor];
+                        } else if (countForCurrRow[currentColor] < countForAnswer[currentColor])
+                        {
+                            numOKGuess += countForCurrRow[currentColor];
+                        }
+                    }
+                }
+
+                //this changes an OKGuess to a PerfectGuess if the color from player's row is also in the correct position
+                int i = 0;
                 while (i < hiddenAnswer.Length)
                 {
-              
                     if (currentPlayerRow[i].ForeColor.Equals(hiddenAnswer[i]))
                     {
-                        //if the color in the player's guess has not been seen before, place it in the isSeen array
-                        if (!colorsSeen.Contains(currentPlayerRow[i].ForeColor))
-                        {
-                            colorsSeen[i] = currentPlayerRow[i].ForeColor;
-                        }
-                        //if the color has been seen elsewhere in the player's guess and numOKGuess was incremented for it, 
-                        //decrement it bc the color is not an OkGuess; it is a PerfectGuess
-                        else if (colorsSeen.Contains(currentPlayerRow[i].ForeColor))
-                        {
-                            numOKGuess--;
-                        }
                         numPerfectGuess++;
-                    }
-                    //if the color is in the answer and also is not seen anywhere else in the player's guess, increment okGuess
-                    else if (hiddenAnswer.Contains(currentPlayerRow[i].ForeColor)
-                             && !colorsSeen.Contains(currentPlayerRow[i].ForeColor))      //if hiddenAnswer contains the color but not necessarily at the right position
-                    {
-                        colorsSeen[i] = currentPlayerRow[i].ForeColor;
-                        numOKGuess++;
+                        numOKGuess--;
                     }
                     i++;
                 }
 
             }
+
+            countForCurrRow.Clear();  //clears all key, value pairs so it can be used for next row
             return numPerfectGuess;
+        }
+
+        //Puts the colors from hiddenAnswer into a Dictionary along with a count of how many times the color appears
+        public void populateCountForAnswer(Color[] hiddenAnswer)
+        {
+            foreach(Color c in hiddenAnswer)
+            {
+                int count = 1;
+                //if color is already in the Dictionary, increment and set its count
+                if (countForAnswer.ContainsKey(c))
+                {
+                    int currentCount;
+                    countForAnswer.TryGetValue(c, out currentCount);
+                    countForAnswer[c] = currentCount + 1;
+                }
+                else
+                {
+                    countForAnswer.Add(c, count);
+                }
+            }
+        }
+
+
+        //Puts the colors from currentPlayerRow in a Dictionary along with a count of how many times that color was placed
+        public void populateCountForCurrRow(Button[] currPlayerRow)
+        {
+            foreach (Button b in currPlayerRow)
+            {
+                Color c = b.ForeColor;
+                int count = 1;
+                if (!countForCurrRow.ContainsKey(c))
+                {
+                    countForCurrRow.Add(c, count);
+                }
+                else
+                {
+                    //if color is already in the Dictionary, increment and set its count
+                    int currentCount;
+                    countForCurrRow.TryGetValue(c, out currentCount);
+                    countForCurrRow[c] = currentCount + 1;
+                }
+            }
         }
 
 
